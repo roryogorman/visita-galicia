41d0984ec7c55e21b29b3a734fca572a22ad273f
roryogorman <roryogorman@hotmail.com>
2021-03-20 19:52:44 +0000

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+/* Show selected province information */
+/* The code for this was sourced from https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp */
+
+function myToggleFunction() {
+  var province = document.getElementById("coruna");
+  if (province.style.display === "none") {
+    province.style.display = "block";
+  } else {
+    province.style.display = "none";
+  }
+}
+
+function myToggleFunction() {
+  var province = document.getElementById("pontevedra");
+  if (province.style.display === "none") {
+    province.style.display = "block";
+  } else {
+    province.style.display = "none";
+  }
+}
+
+function myToggleFunction() {
+  var province = document.getElementById("lugo");
+  if (province.style.display === "none") {
+    province.style.display = "block";
+  } else {
+    province.style.display = "none";
+  }
+}
+
+function myToggleFunction() {
+  var province = document.getElementById("ourense");
+  if (province.style.display === "none") {
+    province.style.display = "block";
+  } else {
+    province.style.display = "none";
+  }
+}
